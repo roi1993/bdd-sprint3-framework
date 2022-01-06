443f32c14561989b8bef45dadae2d0e97d746b01
Kateryna Roi <kateryna782@gmail.com>
2022-01-06 16:34:07 -0500

    api2

--- src/test/java/apiTests/DuoBankAPI.java
+++ src/test/java/apiTests/DuoBankAPI.java
@@ -1,6 +1,9 @@
 package apiTests;
 
+import org.junit.Assert;
 import org.junit.Test;
+import utilities.Driver;
+
 import static io.restassured.RestAssured.*;
 import static io.restassured.matcher.RestAssuredMatchers.*;
 import static org.hamcrest.Matchers.*;
@@ -8,31 +11,73 @@ import static org.hamcrest.Matchers.*;
 public class DuoBankAPI {
 
 
-    int x=100+ (int) (Math.random()*900);
+    int x = 100 + (int) (Math.random() * 900);
 
     @Test
 
-    public void testPostAPI(){
+    public void testPostAPI__PositiveScenarioWithValidRequiredParameters() {
 
 
-        baseURI ="http://qa-duobank.us-east-2.elasticbeanstalk.com/api";
-                given().
+        baseURI = "http://qa-duobank.us-east-2.elasticbeanstalk.com/api";
+        given().
                 header("Accept", "application/json").
-                        body("{\n" +
-                                "  \"first_name\": \"Duotech\",\n" +
-                                "  \"last_name\": \"Academy\",\n" +
-                                "  \"email\": \""+x+"duo@gmail.com\",\n" +
-                                "  \"password\": \"duotech2021\"\n"+
-                                "}").
-                when(). log().all().
-                post("/register.php ").
-                then(). log().all().
+                body("{\n" +
+                        "  \"first_name\": \"Duotech\",\n" +
+                        "  \"last_name\": \"Academy\",\n" +
+                        "  \"email\": \"" + x + "duo@gmail.com\",\n" +
+                        "  \"password\": \"duotech2021\"\n" +
+                        "}").
+                when().log().all().
+                post("/register.php").
+                then().log().all().
                 assertThat().
-                statusCode(200);
-    }
-
-//////
-    /////
+                statusCode(200).
+                body("message", equalTo("You have successfully registered."));
 
     }
+
+
+    @Test
+    public void testPostAPI__negativeSceanrioMissingBody(){
+        baseURI = "http://qa-duobank.us-east-2.elasticbeanstalk.com/api";
+
+
+        given().
+                header("Accept", "application/json").
+                when().log().all().
+                post("/register.php").
+                then().log().all().
+                statusCode(200).
+                body("status", equalTo(422));
+
+
+    }
+
+    @Test
+
+    public void testPostAPI__NegativeScenarioWithValidRequiredParameters() {
+
+
+        baseURI = "http://qa-duobank.us-east-2.elasticbeanstalk.com/api";
+        given().
+                header("Accept", "application/json").
+                body("{\n" +
+                        "  \"first_name\": \"Duotech\",\n" +
+                        "  \"last_name\": \"Academy\",\n" +
+                        "  \"email\": \"duotech@gmail.com\",\n" +
+                        "  \"password\": \"duotech2021\"\n" +
+                        "}").
+                when().log().all().
+                post("/register.php").
+                then().log().all().
+                assertThat().
+                statusCode(200).
+                body("message", equalTo("This E-mail already in use!")).
+                body("status",equalTo(422));
+
+    }
+
+
+
+}
 
