005455c125785e6c4283ab5aa659ec8baed6eedb
Zebo Nur <znuri1991@gmail.com>
2022-01-09 15:14:42 -0500

    Zee API Test-3

--- src/test/java/apiTests/GETMortgageAPI.java
+++ src/test/java/apiTests/GETMortgageAPI.java
@@ -50,6 +50,42 @@ public class GETMortgageAPI {
     }
 
 
+    @Test
+    public void testGETMortgageAPI_logInWithAdminAcct() {
+        baseURI = "http://qa-duobank.us-east-2.elasticbeanstalk.com/api";
+        JsonPath jsonPath = given().
+                header("Accept", "application/json").
+                body("{\n" +
+                        "  \"email\": \"duotechb5@gmail.com\",\n" +
+                        "  \"password\": \"duotechb5\"\n" +
+                        "}").
+                when().log().all().
+                post("/login.php").
+                then().log().all().
+                assertThat().
+                statusCode(200).
+                body("message", equalTo("You have successfully logged in.")).extract().jsonPath();
+
+        String token = jsonPath.getString("token");
+
+        JsonPath jsonPath1 = given().
+                header("Authorization",token).
+                when().log().all().
+                get("/getmortagage.php").
+                then().log().all().
+                //assertThat().
+                        statusCode(200).
+                        body("success",equalTo(1)).extract().jsonPath();
+
+        List<Object> mortagage_applications = jsonPath1.getList("mortagage_applications");
+
+
+        Assert.assertEquals(295,mortagage_applications.size());
+    }
+
+
+
+
 
 
 
